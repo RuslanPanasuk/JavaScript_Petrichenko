092090a520a7357bb391c6ede452d707e11ec9b7
RuslanPanasuk <panasuk.eetp@gmail.com>
2021-12-12 01:12:43 +0200

    feat: add lesson 33 solution

--- Marsian/js/script.js
+++ Marsian/js/script.js
@@ -12,43 +12,107 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
+/* Задания на урок:
+
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) 
+
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
 
-const adv = document.querySelectorAll('.promo__adv img');
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    const adv = document.querySelectorAll('.promo__adv img');
+    
+    adv.forEach(item => {
+        item.remove();
+    });
+    
+    const genre = document.querySelector('.promo__genre');
+    
+    genre.textContent = 'Драма';
+    
+    const promoBackground = document.querySelector('.promo__bg');
+    
+    promoBackground.style.backgroundImage = "url('./img/bg.jpg')";
+    
+    const interactiveList = document.querySelector('.promo__interactive-list');
+    
+    function clearMovies() {
+        interactiveList.innerHTML = '';
+    }
+    
+    function addMoviesFromDB() {
+        clearMovies();
+        movieDB.movies.sort();
+        movieDB.movies.forEach((item, i) => {
+            let newElem = `<li class="promo__interactive-item">${i+1} ${item}<div class="delete"></div></li>`;
+            interactiveList.insertAdjacentHTML('beforeend', newElem);
+        });
 
-adv.forEach(item => {
-    item.remove();
-});
-
-const genre = document.querySelector('.promo__genre');
-
-genre.textContent = 'Драма';
-
-const promoBackground = document.querySelector('.promo__bg');
-
-promoBackground.style.backgroundImage = "url('./img/bg.jpg')";
-
-const interactiveList = document.querySelector('.promo__interactive-list');
-const interactiveListItems = interactiveList.querySelectorAll('li');
-
-interactiveListItems.forEach(item => {
-    item.remove();
-});
-
-movieDB.movies.sort();
-
-movieDB.movies.forEach((item, i) => {
-    let newElem = `<li class="promo__interactive-item">${i+1} ${item}<div class="delete"></div></li>`;
-    interactiveList.insertAdjacentHTML('beforeend', newElem);
+        document.querySelectorAll('.delete').forEach((item, i) => {
+            item.addEventListener('click', () => {
+                item.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                addMoviesFromDB();
+            });
+        });
+    }
+    
+    addMoviesFromDB();
+       
+    
+    
+    const addForm = document.querySelector('form.add');
+    const inputFilm = document.querySelector('.adding__input');
+    //const submitBtn = document.querySelector('button');
+    const checkboxInput = document.querySelector('input[type="checkbox" i]');
+    
+    
+    function addMoviesToDB(film) {
+        if (film.length > 21) {
+            movieDB.movies.push(`${film = film.toUpperCase().slice(0, 21)}...`);
+        } else {
+            movieDB.movies.push(film.toUpperCase());
+        };
+        addMoviesFromDB(); 
+    }
+    
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let film = inputFilm.value;
+        if (checkboxInput.checked) {
+            console.log("Добавляем любимый фильм");
+        };
+        if (film) {
+            addMoviesToDB(film);
+        };    
+        //очистка формы
+        addForm.reset();
+    });
+    
+    
+        
 })
-
