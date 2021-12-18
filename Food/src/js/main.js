c1524ca2cb6b9ede0091621cfcc0ed4e1bbf5ab7
RuslanPanasuk <panasuk.eetp@gmail.com>
2021-12-18 19:50:48 +0200

    feat: add modal functional

--- Food/src/js/main.js
+++ Food/src/js/main.js
@@ -94,4 +94,53 @@ document.addEventListener('DOMContentLoaded', () => {
 
   setClocks('.timer', deadLine);
 
+  // modal
+
+  const modalTrigger = document.querySelectorAll('[data-modal]');
+  const modalCloseBtn = document.querySelector('[data-close]');
+  const modal = document.querySelector('.modal');
+
+  function showModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
+  }
+
+  function closeModal() {
+    modal.classList.remove('show');
+    modal.classList.add('hide');
+    document.body.style.overflow = '';
+  }
+
+  modalTrigger.forEach( item => {
+    item.addEventListener('click', showModal);
+  });
+
+  modalCloseBtn.addEventListener('click', closeModal);
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+      closeModal();
+    }
+  });
+
+  const modalTimerId = setTimeout(showModal, 360000);
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      showModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
+
 })
